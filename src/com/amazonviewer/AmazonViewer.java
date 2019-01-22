f704860802193d8cabaa775f4f317256fe307644
jcorvera <javier.corvra@gmail.com>
2019-01-21 20:05:50 -0600

    Proyecto base

---
+++ src/com/amazonviewer/AmazonViewer.java
@@ -0,0 +1,25 @@
+package com.amazonviewer;
+
+public class AmazonViewer {
+
+	public static void main(String[] args) {
+		
+		int opcion = 0; 
+		
+		do {
+			
+			System.out.println("¡ BIENVENIDOS A AMAZON VIEWER !");
+			System.out.println("");
+			System.out.println("Seleccona el número de la opción deseada ");
+			System.out.println("1. Movies ");
+			System.out.println("2. Series");
+			System.out.println("3. Books");
+			System.out.println("4. Magazines");
+			System.out.println("");
+			
+		}while(opcion !=0);
+		
+
+	}
+
+}
